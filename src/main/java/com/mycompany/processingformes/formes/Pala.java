c148fcb75ffb013889909e7f6697785aae5184cc
kolos <kolos@DESKTOP-GPRSFNC>
2018-11-01 15:01:26 +0100

    Игра Арканоид где мачик должен отбиваться ракеткой, если нет то вычитается жизнь, а так же должны быть блоки при столкновении мяча с ними, они изчезают и прибавляются баллы.

---
+++ src/main/java/com/mycompany/processingformes/formes/Pala.java
@@ -0,0 +1,35 @@
+package com.mycompany.processingformes.formes;
+
+import java.awt.Color;
+import java.awt.Point;
+import processing.core.PApplet;
+
+/**
+ *
+ * @author kolos
+ */
+public class Pala extends Rectangulo {
+    //final static int ALTURA = papplet.height - 20;
+    
+    public Pala(PApplet papplet, Color c, Point p, float anchura, float altura) {
+        super(papplet, c, p, anchura, altura);
+    }
+    
+    public void move(){
+        int xP = papplet.mouseX;
+            if(xP >= papplet.width-(int)getAnchura()){
+               xP = papplet.width-(int)getAnchura();
+            } 
+           
+        int yP = papplet.height-(int)getAltura();
+        
+        
+        setP(new Point(xP, yP));
+                //{
+          // this.p = xP;
+           //this.yP = yP;
+    //}
+        
+        
+    }
+}
