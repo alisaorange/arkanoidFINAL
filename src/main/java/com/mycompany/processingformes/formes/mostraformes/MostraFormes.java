059b94f825141f9a495344466ff1584c669629f8
kolos <kolos@DESKTOP-GPRSFNC>
2018-11-05 19:02:05 +0100

    Игра Арканоид где мачик должен отбиваться ракеткой, если нет то вычитается жизнь, а так же должны быть блоки при столкновении мяча с ними, они изчезают и прибавляются баллы.

--- src/main/java/com/mycompany/processingformes/formes/mostraformes/MostraFormes.java
+++ src/main/java/com/mycompany/processingformes/formes/mostraformes/MostraFormes.java
@@ -6,9 +6,10 @@
 package com.mycompany.processingformes.formes.mostraformes;
 
 import com.mycompany.processingformes.formes.Ball;
+import com.mycompany.processingformes.formes.Blocks;
 import com.mycompany.processingformes.formes.Circle;
 import com.mycompany.processingformes.formes.Pala;
-import com.mycompany.processingformes.formes.Rectangulo;
+//import com.mycompany.processingformes.formes.Rectangulo;
 import java.awt.Color;
 import java.awt.Point;
 import processing.core.PApplet;
@@ -21,11 +22,19 @@ public class MostraFormes extends PApplet {
 
     private Circle circle;
     private Pala pala;
-    private Ball ball;
+    private Blocks blocks;
+    
+    private Blocks blocks1;
+    //String message = "this text is spinning";
+    
+    private int points = 0;
+    
+    private double lifes = 10;
+    
 
     @Override
     public void settings() {
-        size(800, 600);
+        size(800, 500);
 
     }
 
@@ -34,6 +43,9 @@ public class MostraFormes extends PApplet {
         circle = new Ball(this, Color.red, new Point(100, 100), 20);
         pala = new Pala(this, Color.BLUE, new Point(150, 150), 100, 20);
         background(0);
+        blocks = new Blocks(this, Color.BLUE, new Point(100, 100), 200, 10);
+        blocks1 = new Blocks(this, Color.BLUE, new Point(450, 100), 200, 10);
+        
     }
 
     @Override
@@ -42,15 +54,53 @@ public class MostraFormes extends PApplet {
         circle.dibuixa();
         pala.move();
         pala.dibuixa();
+        blocks.dibuixa();
+        blocks1.dibuixa();
+        
+         text("Points you have: ",30,20);
+         text(points,130,20);
+         
+         text("Lifes you have: ",30,40);
+         text((int)lifes,130,40);
+   
+          
+        //System.out.println(pala.getP());
+       // System.out.println(circle.getP());
+       // System.out.println(circle.getSpeedx());
 
-        System.out.println(pala.getP());
-        System.out.println(circle.getP());
-        System.out.println(circle.getSpeedx());
-
-       if(pala.getP().getX() == circle.getP().getX()){
-         circle.setSpeedx(circle.getSpeedx() * -1);
+     //условие если шар сталкивается с ракеткой то отталкивается
+    if((circle.getP().getX() > pala.getP().getX()) 
+            && (circle.getP().getX() < pala.getP().getX() + 100) 
+            && circle.getP().getY() + circle.getRadi() == pala.getP().getY()){
+                 circle.setSpeedy(circle.getSpeedy()+1);
+                 circle.setSpeedx(circle.getSpeedx()+1);
+                 circle.setSpeedy(circle.getSpeedy() * -1);
           }
+    
+    
+    //условие если мяч косается блока, он пропалает 
+    if((circle.getP().getX() >= blocks.getP().getX()) 
+            && (circle.getP().getX() <= blocks.getP().getX() + 200) 
+            && circle.getP().getY() + circle.getRadi() == blocks.getP().getY() + 10){
+              //блок исчезает 
+              //blocks.setC(Color.black);
+              blocks.setP(new Point(0, -200));
+              points = points +1;  //при попадание по блоку +1 балл
+          }
+    //Блок 2. Надо придумать как создать много блоков (использовать цикл for)
+     if((circle.getP().getX() >= blocks1.getP().getX()) 
+            && (circle.getP().getX() <= blocks1.getP().getX() + 200) 
+            && circle.getP().getY() + circle.getRadi() == blocks1.getP().getY() + 10){
+              //блок исчезает 
+              blocks1.setP(new Point(0, -200));
+              points = points +1;
+          }
+    //Если мяч не попадает по ракетке -1 жизнь.
+    if(circle.getP().getY() + circle.getRadi() == height){
+        lifes = lifes - 0.5; //почему то вычиет 2 жизни, разобраться!!!
     }
+  }
+    
 
     public static void main(String[] args) {
         String[] processingArgs = {"Pilota"};
